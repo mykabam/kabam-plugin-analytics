3eb30e490c1ea3822f8e2f5f74049b17501e342c
T. Budiman <tbudiman@gmail.com>
2013-08-10 18:11:20 +0700

    feat(transient): transient store

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
 var url = require('url'),
+  useragent = require('express-useragent'),
   transientAnalytics = require('./lib/transientAnalytics'),
-  persistentAnalytics = require('./lib/persistentAnalytics');
+  persistentAnalytics = null;
 
 exports.model = {
   Total: require('./models/total').modelFactory,
   TotalMinute: require('./models/totalMinute').modelFactory
 };
 
-exports.core = function(mwc) {
-  var hostUrl = mwc.config.hostUrl ? mwc.config.hostUrl : 'localhost',
-    site = url.parse(hostUrl).host;
+exports.middleware = [
+  function(mwc) {
+    return useragent.express();
+  },
+  function(mwc) {
+    var hostUrl = mwc.config.hostUrl ? mwc.config.hostUrl : 'http://localhost',
+      site = url.parse(hostUrl).host;
 
-  mwc.analytics = {};
-
-  mwc.analytics.transientAnalytics = transientAnalytics;
-  mwc.analytics.transientAnalytics.useRedis(mwc.redisClient);
-
-  mwc.analytics.transientAnalytics.site = site;
-
-  mwc.analytics.persistentAnalytics = persistentAnalytics;
-};
-
-exports.middleware = function(mwc) {
-  return mwc.analytics.transientAnalytics.store;
-};
+    transientAnalytics.useRedis(mwc.redisClient);
+    // persistentAnalytics = require('./lib/persistentAnalytics');
+    return transientAnalytics.store;
+  }
+];
 
 exports.routes = function(mwc) {
-  mwc.app.get('/analytics/hotpixel.png', function(request, response) {
+  mwc.app.get('/analytics/:site/hotpixel.png', function(request, response) {
     response.set('Cache-Control', 'no-cache, no-store, must-revalidate');
     response.set('Pragma', 'no-cache');
     response.set('Expires', 0);
-    response.sendfile('./public/img/hotpixel.png');
+    response.sendfile(__dirname + '/public/img/hotpixel.png');
   });
 };
