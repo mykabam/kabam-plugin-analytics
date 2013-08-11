13794e40ab6babc81e8498162fbc22f7bebae2e6
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-08-11 13:55:41 +0400

    [fix] - more usefull hotpixel and some insights how to make it more usefull

--- index.js
+++ index.js
@@ -25,7 +25,29 @@ exports.middleware = [
 ];
 
 exports.routes = function(mwc) {
-  mwc.app.get('/analytics/:site/hotpixel.png', function(request, response) {
+  mwc.app.get(/^\/analytics\/([\/0-9a-z\.]+)\/hotpixel.png$/, function(request, response) {
+    //we react on site name like this
+    /*
+     /analytics/somesite.org/mega/hotpixel.png -> somesite.org/mega
+     /analytics/somesite.org/hotpixel.png -> somesite.org
+     /analytics/localhost/hotpixel.png -> localhost
+    */
+
+    var siteName = request.params[0]; // this is sitename parsed from url, we can process it
+    var referrerUrl = request.header('Referer'); //http://expressjs.com/api.html#req.get
+
+
+    //for hitpixels referer is USUALLY the URL of page, where it is placed,
+    //if referrer !=siteName - means somebody places referrer on 3rd party site to fake the statistics
+
+    var refregex=new RegExp('/'+siteName+'/','i');
+    if(refregex.test(referrerUrl)){
+      //this is correct hotpixel
+    } else {
+      //this is cheating!
+    }
+
+
     response.set('Cache-Control', 'no-cache, no-store, must-revalidate');
     response.set('Pragma', 'no-cache');
     response.set('Expires', 0);
